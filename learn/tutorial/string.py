c5b7fe3ca32c2955a2d300ad0898688498a4229a
vincentlee <vincentlee@usa.com>
2018-11-26 22:52:00 +0800

    add regex for ()

--- learn/tutorial/string.py
+++ learn/tutorial/string.py
@@ -17,4 +17,19 @@ print('3.1415926'.zfill(5))
 print('3.1315926'.ljust(5)[:5])
 print('3.1315926'.ljust(5))
 
+def GetMiddleStr(content,startStr,endStr):
+  startIndex = content.index(startStr)
+  if startIndex>=0:
+    startIndex += len(startStr)
+  endIndex = content.index(endStr)
+  return content[startIndex:endIndex]
 
+ print(GetMiddleStr('<div class="a">jb51.net</div>','<div class="a">','</div>'))
+ print(GetMiddleStr('    _T("liwenxing")', '_T("', '")'))
+
+ import re
+string = '_T("liwenxing("ll")sdfs")'
+p1 = re.compile(r'[(](.*?)[)]', re.S) #最小匹配
+p2 = re.compile(r'[(](.*)[)]', re.S)  #贪婪匹配
+print(re.findall(p1, string))
+print(re.findall(p2, string))
